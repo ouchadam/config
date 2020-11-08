0fc925f754e9e555f36c8d5d531713d733fe6edd
Adam Brown <adampsbrown@gmail.com>
2020-11-08 22:27:01 +0000

    in progress

---
+++ arch/init-wm.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# base
+
+sudo pacman -S \
+  wget \
+  git \
+  base-devel \
+  --needed \
+  --no confirm
+
+
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd -
+rm -rf yay
+
+
+# install WM packages
+
+sudo pacman -S \
+  sway \
+  alacritty \
+  wofi \
+  xorg-server-xwayland \
+  --noconfirm
+
+# configure
+
+mkdir -p ~/.config/sway/
+wget -o  ~/.config/sway/config.in https://github.com/ouchadam/config/blob/main/sway/config.in
+
+
+# install various
+
+sudo yay -S \
+  google-chrome \
+  --noconfirm
+  
